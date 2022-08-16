70b76d03a5c0ca9ec68e35ecea4e6d908c89aea9
Thomahawkuru <thomas.boer.rc@gmail.com>
2022-08-16 13:54:32 +0200

    added plot folder and histogram plots

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import os
 import matplotlib.pyplot as plt
 from plotly.offline import plot
 import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
 import numpy as np
 import readers
 import calculators
@@ -49,6 +51,11 @@ for p in range(1, participants + 1):
     completion_times[p]   = readers.times(datapath, p)
     handedness[p]         = readers.handedness(datapath,p)
     
+#%%plot data
+data = np.array(list(completion_times.items()))
+fig1 = ff.create_distplot([data[:,1]], ['completion times [s]'])
+plot(fig1)
+
 #%% Velocity
 input_velocity = {}
 
@@ -59,13 +66,15 @@ for p in range(1, participants + 1):
         input_velocity[p] = calculators.velocity(hand_data[p], columns_L)
     
 #%% plot data
+fig1 = go.Figure()
+
 for p in range(1, participants + 1):
-    plt.plot(hand_data[p].t,input_velocity[p])
+   fig1.add_trace(go.Scatter(x=hand_data[p].t, y=input_velocity[p], name=str(p), mode="lines"))
     
-plt.show()
+plot(fig1)
     
     
 #plt.plot(hand_data[1].LPos_X, hand_data[1].LPos_Z) 
-fig = px.line_3d(hand_data[1], x='LPos_Z', y='LPos_X', z='LPos_Y', title = 'Hand input')
-plot(fig)
+#fig2 = px.line_3d(hand_data[1], x='LPos_Z', y='LPos_X', z='LPos_Y', title = 'Hand input')
+#plot(fig2)
 #input_paths         = calcualtors.input_paths(timestamps, hand_data)
