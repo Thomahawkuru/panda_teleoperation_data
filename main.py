8c97b9fe01952d499961927407877b564245cc77
Thomas de Boer <thomas.boer.rc@gmail.com>
2022-12-08 13:02:07 +0100

    changed to read experiment data structure. under construction

--- main.py
+++ main.py
@@ -14,29 +14,34 @@ import plotly.figure_factory as ff
 import numpy as np
 import readers
 import calculators
-import pandas as pd
 
 #%% define variables --------------------------------------------------------------------------------------------------
-datapath        = os.getcwd() + "\\data\\"        # full path to read recorded data
+datapath        = os.getcwd() + "\\data\\Experiment\\"        # full path to read recorded data
 savepath        = os.getcwd() + "\\save\\"        # full path to save calculated data
-participants    = [0]                            # number of participants
-conditions      = ['A', 'B', 'C', 'D', 'E', 'F']
+
+Participants    = [1]                            # number of participants
+Conditions      = ['A', 'B', 'C', 'D', 'E', 'F']
+Trials          = [1, 2, 3]
+Files           = ['experiment', 'gaze', 'hand', 'hmd', 'gripper', 'robot']
+
 columns_R       = ['RPos_X', 'RPos_Y', 'RPos_Z']
 columns_L       = ['LPos_X', 'LPos_Y', 'LPos_Z']
 
 exp_header      = [ "f", "dt", "t", "fps", "start", "hand", "Grip", "Tracked", "Controlling" ];
 gaze_header     = [ "f", "dt", "t", "status", "gazeX", "gazeY", "gazeZ", "gazeposition", "focusdistance", "focusstability", "statusL", "LeftX", "LeftY", "LeftZ", "posL", "pupilL", "statusR", "RightX", "RightY", "RightZ", "positionR", "pupilR"];
-hand_header     = [ "f", "dt", "t", "LposX", "LposY", "LposZ", "LrotX", "LrotY", "LrotZ", "LrotW", "Lgrab", "Lpinch", "RposX", "RPosY", "RposZ", "RrotX", "RrotY", "RrotZ", "RrotW", "Rgrab", "Rpinch"];
+hand_header     = [ "f", "dt", "t", "posX", "posY", "posZ", "rotX", "rotY", "rotZ", "rotW", "grab", "pinch", "CMDposX", "CMDPosY", "CMDposZ", "CMDrotX", "CMDrotY", "CMDrotZ", "CMDrotW"];
 hand_hdr_short  = [ "f", "dt", "t", "posX", "posY", "posZ", "rotX", "rotY", "rotZ", "rotW", "grab", "pinch"];
 hmd_header      = [ "f", "dt", "t", "posX", "posY", "posZ", "rotX", "rotY", "rotZ"];
 gripper_header  = [ "f", "dt", "t", "posX", "posY", "posZ", "rotX", "rotY", "rotZ", "rotW", "grip_pos", "grip_vel", "grip_eff" ];
 robot_header    = [ "f", "dt", "t", "tau1", "tau2", "tau3", "tau4", "tau5", "tau6", "tau7", "con_x", "con_y", "con_z" ];
-    
+
+files = dict(zip(Files, [None]*len(Files)))
+trials = dict(zip(Trials, [files]*len(Trials)))
+conditions = dict(zip(Conditions, [trials]*len(Conditions)))
+data = dict(zip(Participants, [conditions]*len(Participants)))    
 
 #%% Read data ----------------------------------------------------------------------------------------------------------------------
-
-for p in participants:
-    handedness       = {}
+for p in Participants:
     exp_data         = {}
     gaze_data        = {}
     hand_data        = {}
@@ -46,35 +51,24 @@ for p in participants:
     time             = {} 
     
     print(), print(), print('Reading data for participant {}'.format(p))    
-    handedness[p]       = readers.handedness(datapath,p)
-    print('Handedeness {}'.format(handedness[p]))
     
-    for c in conditions:     
+    for c in Conditions:     
         
         print(), print('Condition {}'.format(c))
         if c == "A": 
             continue
-        
-        exp_data[c]         = readers.csv(datapath, p, c, 'Experiment.csv', exp_header)
-        gaze_data[c]        = readers.csv(datapath, p, c, 'Gaze.csv', gaze_header)
-        hand_data[c]        = readers.csv(datapath, p, c, 'Hand.csv', hand_header)
-        hmd_data[c]         = readers.csv(datapath, p, c, 'HMD.csv', hmd_header)
-        gripper_data[c]     = readers.csv(datapath, p, c, 'Gripper.csv' , gripper_header)
-        robot_data[c]       = readers.csv(datapath, p, c, 'Robot.csv' , robot_header)
-        
-        time[c]             = hand_data[c].t
-       
-#organize data based on handedness    
-        if handedness[p] == 'R':
-            hand_data[c].drop(list(hand_data[c].filter(regex = 'L')), axis=1, inplace=True)
-            hand_data[c].columns = hand_hdr_short
+        for t in Trials:    
+            print('Trial {}'.format(t))            
+            data[p][c][t]['experiment'] = readers.csv(datapath, p, c, t, 'Experiment.csv', exp_header)
+            data[p][c][t]['gaze']       = readers.csv(datapath, p, c, t,'Gaze.csv', gaze_header)
+            data[p][c][t]['hand']       = readers.csv(datapath, p, c, t, 'Hand.csv', hand_header)
+            data[p][c][t]['hmd']        = readers.csv(datapath, p, c, t, 'HMD.csv', hmd_header)
+            data[p][c][t]['gripper']    = readers.csv(datapath, p, c, t, 'Gripper.csv' , gripper_header)
+            data[p][c][t]['robot']      = readers.csv(datapath, p, c, t, 'Robot.csv' , robot_header)
             
-        elif handedness[p] == 'L':
-            hand_data[c].drop(list(hand_data[c].filter(regex = 'R')), axis=1, inplace=True)
-            hand_data[c].columns = hand_hdr_short
-
-    
-        #%% plot input path    
-        plt.plot(time[c], hand_data[c].posZ) 
-        fig3 = px.line_3d(hand_data[c], x='posZ', y='posX', z='posY', title = 'Hand input')
-        plot(fig3, filename='plots/fig3.html')
+            time                        = data[p][c][t]['hand'] 
+              
+            #%% plot input path    
+            plt.plot(time, data[p][c][t]['hand'].posZ) 
+            fig3 = px.line_3d(data[p][c][t]['hand'], x='posZ', y='posX', z='posY', title = 'Hand input')
+            plot(fig3, filename='plots/fig{}{}{}.html'.format(p,c,t,))
--- readers.py
+++ readers.py
@@ -11,24 +11,19 @@ import math
 import functions
 import numpy as np
 
-def handedness(path,participant):
-    participant_folder = [i for i in os.listdir(path) if i.startswith(str(participant))]
-    hand = participant_folder[0][2]
-        
-    return hand
-
-def csv(path, participant, condition, filename, header):
+def csv(path, participant, condition, trial, filename, header):
     # read data file
   
-    participant_folder = [i for i in os.listdir(path) if i.startswith(str(participant))]
-    participant_path = path + '{}\\'.format(participant_folder[0])   
+    participant_path = path + '{}\\'.format(str(participant))   
     
     trial_folder = [i for i in os.listdir(participant_path) if i.startswith(str(condition + '_'))]
-    trial_path = participant_path + '{}\\'.format(trial_folder[0]) 
+    trial_path = participant_path + '{}\\'.format(trial_folder[trial-1]) 
+
+
     
     file = [i for i in os.listdir(trial_path) if os.path.isfile(os.path.join(trial_path, i)) and filename in i]
     
-    csvdata = pandas.read_csv(trial_path + file[0], delimiter=",", header=0, names=header)
+    csvdata = pandas.read_csv(trial_path + file[0], delimiter=",", header=0, names=header, skipfooter=1, engine='python')
 
     return csvdata
 
