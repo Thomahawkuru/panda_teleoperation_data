5668d52033bbb75314b958111283ded9eb64cab0
Thomahawkuru <thomas.boer.rc@gmail.com>
2022-12-16 15:57:37 +0100

    Updated plotting and implemented saving

--- plot.py
+++ plot.py
@@ -1,37 +1,70 @@
 #%% import
-import os
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from plotly.offline import plot
 import plotly.express as px
-import plotly.graph_objects as go
-import plotly.figure_factory as ff
-import numpy as np
-import pickle
 import dill
-import functions
 
 dill.load_session('data_calculated.pkl')
 
 #%% plot sanity check data    
-fpss =[]
+fpss = []
 times = []
 
-for p in Participants:
+for c in Participants:
     for c in Conditions[1:]:
         for t in Trials:
-            fpss.append(data[p][c][t]['FPS'])
-            times.append(data[p][c][t]['Duration'])
-            
+            fpss.append(data[p][c][t]['fps'])
+            times.append(data[p][c][t]['duration'])        
 
-plt.boxplot([fpss,times])
+fig1, ax1 = plt.subplots()
+ax1.boxplot([fpss,times])
+ax1.set_title('Sanity Checking, n = [{}]'.format(2*5*3))
+ax1.set_xticklabels(['Average FPS','Duration'])
+fig1.savefig("plots/sanity_check.jpg")
 
-#%%
-for p in Participants:
-    for c in Conditions[1:]:
+#%% plot grab data
+failed_grabs = pd.DataFrame()
+correct_grabs = pd.DataFrame()
+
+for c in Conditions[1:]:
+    fails = []
+    succes = []
+    for p in Participants:
+        failed = 0
+        correct = 0
         for t in Trials:
-            plt.plot(data[p][c][t]['Hand'].posZ) 
-            fig3 = px.line_3d(data[p][c][t]['Hand'], x='posZ', y='posX', z='posY', title = 'Hand input')
-            plot(fig3, filename='plots/fig{}{}{}.html'.format(p,c,t,))
+            failed += data[p][c][t]['grabs']['fail']
+            correct += data[p][c][t]['grabs']['succes']
+        
+        fails.append(failed/3)
+        succes.append(correct/3)
+
+    failed_grabs[c] = fails
+    correct_grabs[c] = succes
+
+fig2, ax2 = plt.subplots()
+ax2.boxplot(failed_grabs)
+ax2.set_title('Failed Grabs [Average n]')
+ax2.set_xlabel('Conditions')
+ax2.set_xticklabels(['B','C','D','E','F'])
+fig2.savefig("plots/failed_grabs.jpg")
+
+fig3, ax3 = plt.subplots()
+ax3.boxplot(correct_grabs)
+ax3.set_title('Correct Grabs [Average n]')
+ax3.set_xticklabels(['B','C','D','E','F'])
+ax3.set_xlabel('Conditions')
+fig3.savefig("plots/correct_grabs.jpg")
+
+#%% plot input paths
+# for p in Participants:
+#     for c in Conditions[1:]:
+#         for t in Trials:
+#             plt.plot(data[p][c][t]['Hand'].posZ) 
+#             fig3 = px.line_3d(data[p][c][t]['Hand'], x='posZ', y='posX', z='posY', title = 'Hand input')
+#             plot(fig3, filename='plots/fig{}{}{}.html'.format(p,c,t,))
 
 # %%
 print(), print('Dumping plotted data to file...')
