e94247bd6330e7cead372950a4a48894890bd917
Thomas de Boer <thomas.boer.rc@gmail.com>
2023-01-25 17:24:38 +0100

    Added data participant 18

--- plot.py
+++ plot.py
@@ -9,7 +9,7 @@ import seaborn as sns
 import functions
 
 dill.load_session('data_calculated.pkl')
-Participants = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]                            # number of participants
+Participants = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]                            # number of participants
 
 #%% plot sanity check data    
 fpss = []
