1427a6a3b9830cf9c298f29bd932ccf0a9e27cb6
Thomahawkuru <thomas.boer.rc@gmail.com>
2023-03-13 19:01:50 +0100

    tried async processing but does currently not work with dill

--- calculate_async.py
+++ calculate_async.py
@@ -7,72 +7,40 @@ import pickle
 import calculators
 import warnings
 import _thread
+import multiprocessing as mp
 
-def calculate_fps(data,p,c,t):
+def process_participant(data,p,c,t):
     data[p][c][t]['fps'] = calculators.fps(data, p, c, t)
-
-def calculate_duration(data,p,c,t):
     data[p][c][t]['duration'] = calculators.duration(data, p, c, t)
-
-def calculate_time(data,p,c,t):
     data[p][c][t]['time'] = calculators.time(data, p, c, t)
-
-def calculate_tracking_err(data,p,c,t):
     data[p][c][t]['track_err'] = calculators.track_error(data, p, c, t)
-
-def calculate_grabs(data,p,c,t):
     data[p][c][t]['grabs'] = calculators.grabs(data, p, c, t, debug=False)
-
-def calculate_velocity(data,p,c,t):
     data[p][c][t]['velocity'] = calculators.grab_velocity(data, p, c, t, 'Hand', pre_time=2, debug=False)
-
-def calculated_hmd(data,p,c,t):
-    if c is not 'B':
+    if c != 'B':
         data[p][c][t]['HMD'] = calculators.head_movement(data, p, c, t, debug=False)
+    data[p][c][t]['in_out'] = calculators.in_out_corr(data, p, c, t, debug=False)
 
-def calculated_corr(data,p,c,t):
-    data[p][c][t]['in_out'] = calculators.in_out_corr(data, p, c, t, debug=False) 
+if __name__ == '__main__':
+    dill.load_session('data_raw.pkl')
+    start = time.time()
+    Participants = [1,2,3,4,6,7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+    
+    with mp.Pool() as pool:
+        for p in Participants:
+            print(), print('Calculating data for participant {}'.format(p))
+            for c in Conditions[1:]:
+                print(), print('Condition {}'.format(c))
+                for t in Trials:
+                    print('Trial {}'.format(t))
+                    params = [(data, p, c, t)]
+                    pool.map(process_participant, params)
+                    while True:
+                        if len(Participants) == 0:
+                            print('All participants have been processed.')
+                            print(), print('Dumping calculated data to file...')
+                            dill.dump_session('data_calculated.pkl')
 
-
-dill.load_session('data_raw.pkl')
-start = time.time()
-Participants    = [1,2,3,4,6,7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]                      # Array of participants
-
-#%% Calculate data   
-for p in Participants:
-    print(), print(), print('Calculating data for participant {}'.format(p))
-    for c in Conditions[1:]:
-        print(), print('Condition {}'.format(c))
-        for t in Trials:
-            print('Trial {}'.format(t))
-            
-            # calculate_fps(data,p,c,t)
-            # calculate_duration(data,p,c,t)
-            # calculate_time(data,p,c,t)
-            # calculate_tracking_err(data,p,c,t)
-            # calculate_grabs(data,p,c,t)
-            # calculate_velocity(data,p,c,t)
-            # calculated_hmd(data,p,c,t)
-            # calculated_corr(data,p,c,t)
-            # calculated_corr(data,p,c,t)  
-
-            try:
-                _thread.start_new_thread( calculate_fps, (data,p,c,t, ) )
-                _thread.start_new_thread( calculate_duration, (data,p,c,t, ) )
-                _thread.start_new_thread( calculate_time, (data,p,c,t, ) )
-                _thread.start_new_thread( calculate_tracking_err, (data,p,c,t, ) )
-                _thread.start_new_thread( calculate_grabs, (data,p,c,t, ) )
-                _thread.start_new_thread( calculate_velocity, (data,p,c,t, ) )
-                _thread.start_new_thread( calculated_hmd, (data,p,c,t, ) )
-                _thread.start_new_thread( calculated_corr, (data,p,c,t, ) )
-                _thread.start_new_thread( calculated_corr, (data,p,c,t, ) )
-
-            except:
-                print("Error: unable to start thread")
-        
-# %%
-print(), print('Dumping calculated data to file...')
-dill.dump_session('data_calculated.pkl')
-
-end =  time.time()
-print("Calculation time: {}".format(end-start))
+                            end =  time.time()
+                            print("Calculation time: {}".format(end-start))
+                            break
+    
---
+++ read_async.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 20 11:14:53 2022
+
+@author: Thomas
+"""
+
+#%% import
+import os
+import time
+import matplotlib.pyplot as plt
+from plotly.offline import plot
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import numpy as np
+import pandas as pd
+import pickle
+import dill
+import functions
+import multiprocessing as mp
+
+# #%% Read data ----------------------------------------------------------------------------------------------------------------------
+# for p in Participants: 
+#     print(), print('Reading data for participant {}'.format(p))    
+    
+#     for c in Conditions[1:]:        
+#         print('Condition {}'.format(c))
+
+#         for t in Trials:           
+#             for f,h in zip(Files, Headers):
+#                 data[p][c][t].update({f: functions.read_csv(datapath, p, c, t, f + '.csv', h)})
+
+def process_file(data, datapath, p, c, t, f, h):
+    data[p][c][t].update({f: functions.read_csv(datapath, p, c, t, f + '.csv', h)})
+    
+if __name__ == '__main__':
+    start = time.time()
+
+    # define variables --------------------------------------------------------------------------------------------------
+    root            = os.getcwd().replace("\\","/")
+    datapath        = root + "/data/Experiment/"        # full path to read recorded data
+    savepath        = root + "/save/"        # full path to save calculated data
+
+    exp_header      = [ "f", "dt", "t", "fps", "start", "hand", "Grip", "Tracked", "Controlling" ]
+    gaze_header     = [ "f", "dt", "t", "status", "gazeX", "gazeY", "gazeZ", "gazeposition", "focusdistance", "focusstability", "statusL", "LeftX", "LeftY", "LeftZ", "posL", "pupilL", "statusR", "RightX", "RightY", "RightZ", "positionR", "pupilR"]
+    hand_header     = [ "f", "dt", "t", "posX", "posY", "posZ", "rotX", "rotY", "rotZ", "rotW", "grab", "pinch", "CMDposX", "CMDPosY", "CMDposZ", "CMDrotX", "CMDrotY", "CMDrotZ", "CMDrotW"]
+    hmd_header      = [ "f", "dt", "t", "posX", "posY", "posZ", "rotX", "rotY", "rotZ", "rotw"]
+    gripper_header  = [ "f", "dt", "t", "posX", "posY", "posZ", "rotX", "rotY", "rotZ", "rotW", "grip_pos", "grip_vel", "grip_eff" ]
+    robot_header    = [ "f", "dt", "t", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "fx", "fy", "fz" ]
+
+    Participants    = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]                      # Array of participants
+    Conditions      = ['A', 'B', 'C', 'D', 'E', 'F']
+    Trials          = [1, 2, 3]
+    Files           = ['Experiment', 'Gaze', 'Hand', 'HMD', 'Gripper', 'Robot']
+    Headers         = [exp_header, gaze_header, hand_header, hmd_header, gripper_header, robot_header]
+
+    data = {Participant: {Condition: {Trial: {File: {} for File in Files} for Trial in Trials} for Condition in Conditions} for Participant in Participants}
+
+    with mp.Pool() as pool:
+        for p in Participants:
+            print(), print('Reading data for participant {}'.format(p))    
+    
+            for c in Conditions[1:]:
+                print('Condition {}'.format(c))
+
+                for t in Trials:
+                    params = [(data, datapath, p, c, t, f, h) for f,h in zip(Files, Headers)]
+                    pool.starmap(process_file, params)  
+
+    #%% save imported data
+    print(), print('Dumping raw data to file...')
+    #dill.dump_session('data_raw.pkl')
+
+    end =  time.time()
+    print("Read time: {}".format(end-start))
