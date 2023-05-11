32d58b076e4e2ae4afd162930d3e6024a04a01e4
Thomas de Boer <thomas.boer.rc@gmail.com>
2023-05-11 13:03:39 +0200

    added sanity check data to eval

--- eval.py
+++ eval.py
@@ -14,7 +14,7 @@ plot_start = time.time()
 dill.load_session('data_evaluated.pkl')
 Participants    = [1,2,3,4,6,7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26] # Array of participants
 
-#%% produce raw grab data
+#%% produce data
 grabs = grabs.sort_values(by=["participant","condition","measure"]).reset_index(drop=True)
 velocity = velocity.sort_values(by=["participant","condition","measure"]).reset_index(drop=True)
 hmd_movement = hmd_movement.sort_values(by=["participant","condition","measure"]).reset_index(drop=True)
@@ -26,3 +26,14 @@ velocity.to_csv("data/eval/velocity.csv")
 hmd_movement.to_csv("data/eval/hmd_movement.csv")
 in_out_corr.to_csv("data/eval/in_out_corr.csv")
 force.to_csv("data/eval/force.csv")
+
+#sanity checking data
+fpss =  fpss.sort_values(by=["participant","condition","trial"]).reset_index(drop=True)
+times =  times.sort_values(by=["participant","condition","trial"]).reset_index(drop=True)
+track_err = track_err.sort_values(by=["participant","condition","trial"]).reset_index(drop=True)
+input_lag = input_lag.sort_values(by=["participant","condition","trial"]).reset_index(drop=True)
+
+fpss.to_csv("data/eval/fpss.csv")
+times.to_csv("data/eval/completion_times.csv")
+track_err.to_csv("data/eval/track_err.csv")
+input_lag.to_csv("data/eval/input_lag.csv")
