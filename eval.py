1a8335e12da7d1dd735981c429299497903a9237
Thomas de Boer <thomas.boer.rc@gmail.com>
2023-05-11 12:56:07 +0200

    added eval data

---
+++ eval.py
@@ -0,0 +1,28 @@
+#%% import
+import time
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from plotly.offline import plot
+import plotly.express as px
+import dill
+import seaborn as sns
+import functions
+
+plot_start = time.time()
+dill.load_session('data_evaluated.pkl')
+Participants    = [1,2,3,4,6,7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26] # Array of participants
+
+#%% produce raw grab data
+grabs = grabs.sort_values(by=["participant","condition","measure"]).reset_index(drop=True)
+velocity = velocity.sort_values(by=["participant","condition","measure"]).reset_index(drop=True)
+hmd_movement = hmd_movement.sort_values(by=["participant","condition","measure"]).reset_index(drop=True)
+in_out_corr = in_out_corr.sort_values(by=["participant","condition","measure"]).reset_index(drop=True)
+force = force.sort_values(by=["participant","condition","measure"]).reset_index(drop=True)
+
+grabs.to_csv("data/eval/grabs.csv")
+velocity.to_csv("data/eval/velocity.csv")
+hmd_movement.to_csv("data/eval/hmd_movement.csv")
+in_out_corr.to_csv("data/eval/in_out_corr.csv")
+force.to_csv("data/eval/force.csv")
